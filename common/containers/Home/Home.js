f6bbbc1ae9655eedf054fbaa1092a81bd97a71df
zwug <zwugme@gmail.com>
2016-12-04 00:02:11 +0300

    fixed video size

--- common/containers/Home/Home.js
+++ common/containers/Home/Home.js
@@ -7,8 +7,21 @@ function Home({ errorMessage, children }) {
   return (
     <div>
       <Header/>
+      <div className={s.videoContainer}>
+        <video className={s.video}
+          src="static/video/promo.mp4"
+          autoPlay
+          loop
+          muted>
+        </video>
+        <div className={s.headingContainer}>
+          <h1 className={s.heading}>Добро пожаловать в Тридевятое!</h1>
+        </div>
+      </div>
       <div className={s.container}>
-        <h1>Добро пожаловать в Тридевятое!</h1>
+        <div className={s.logo}></div>
+        <p>Мы - творческое объединение людей, знающих своё дело.
+           Организовываем сказочные балы любого формата.</p>
       </div>
     </div>
   );
