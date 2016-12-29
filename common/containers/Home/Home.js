d541de3b7d1e6f1926de158180454e19a3298014
zwug <zwugme@gmail.com>
2016-12-29 23:16:58 +0300

    removed video on xs

--- common/containers/Home/Home.js
+++ common/containers/Home/Home.js
@@ -7,35 +7,37 @@ import Header from '../../components/Header/Header'
 import RegisterForm from '../../components/RegisterForm/RegisterForm'
 import * as userActions from '../../actions/user'
 
-function Home({ errorMessage, children, userActions}) {
-  return (
-    <div>
-      <div className={s.videoContainer}>
-        <Header opaque />
+let isMobile = () => /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+
+const Home = ({ errorMessage, children, userActions}) => (
+  <div>
+    <div className={s.videoContainer}>
+      <Header opaque />
+      {!isMobile() &&
         <video className={s.video}
           src="static/video/promo.mp4"
           autoPlay
           loop
           muted>
         </video>
-        <div className={s.headingContainer}>
-          <h1 className={s.heading}>Добро пожаловать в Тридевятое!</h1>
-        </div>
-      </div>
-      <div className={s.container}>
-        <div className={s.logo}></div>
-        <p>Мы - творческое объединение людей, знающих своё дело.
-           Организовываем сказочные балы любого формата.</p>
-        <RegisterForm onSubmit={userActions.register}/>
+      }
+      <div className={s.headingContainer}>
+        <h1 className={s.heading}>Добро пожаловать в Тридевятое!</h1>
       </div>
     </div>
-  );
-}
+    <div className={s.container}>
+      <div className={s.logo}></div>
+      <p>Мы - творческое объединение людей, знающих своё дело.
+         Организовываем сказочные балы любого формата.</p>
+      <RegisterForm onSubmit={userActions.register}/>
+    </div>
+  </div>
+)
 
 Home.propTypes = {
   children: PropTypes.element
   //@TODO add
-};
+}
 
 function mapDispatchToProps(dispatch) {
   return {
