d74db6f4ed83222c99ad56ae66ff7075e64238a9
zwug <zwugme@gmail.com>
2017-01-29 13:29:18 +0300

    added fields to form

--- common/components/RegisterForm/RegisterForm.js
+++ common/components/RegisterForm/RegisterForm.js
@@ -4,6 +4,91 @@ import { Field, reduxForm } from 'redux-form'
 import FormField from '../FormField/FormField'
 import s from './RegisterForm.css'
 
+class RegisterForm extends Component {
+  static propTypes = {
+
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.skillLevels = ['Новичек', 'Любитель', 'Профи']
+    this.balls = [
+      'Весенний бал МГТУ МИРЭА',
+      'Чёрно-белая сказка',
+      'Путешествие в Трансильванию',
+      'Венский бал МИРЭА'
+    ]
+    this.booleanChoises = [
+      'да',
+      'нет'
+    ]
+  }
+
+  handleSubmit = (values) => {
+    values['ball'] = this.balls.reduce((balls, ball, index) => {
+      if (values[`ball-${index}`]) {
+        balls.push(ball)
+      }
+      return balls
+    }, [])
+    console.log(values)
+    this.props.onSubmit(values)
+  }
+
+  render() {
+    const { handleSubmit, pristine, reset, submitting } = this.props
+    return (
+      <form onSubmit={handleSubmit(this.handleSubmit)} noValidate >
+        <Field name="name" type="text" component={FormField} label="ФИО"/>
+        <Field name="date" type="date" component={FormField} label="Дата рождения"/>
+        <Field name="number" type="number" component={FormField} label="Телефон"/>
+        <Field name="email" type="email" component={FormField} label="Email"/>
+        <Field name="vk" component={FormField} label="Ссылка на профиль вконтакте (для добавления в группу)"/>
+        <div className={s.inputHeading}>Уровень танцевальной подготовки</div>
+        {this.skillLevels.map((level, index) => (
+          <label className={s.label} key={index}>
+            <Field className={s.checkInput} name="level" component="input" type="radio" value={`${index}`}>
+            </Field>
+            {level}
+          </label>
+        ))}
+        <div className={s.inputHeading}>Если вы были на балах МИРЭА, отметьте на каких</div>
+        {this.balls.map((ball, index) => (
+          <label className={s.label} key={index}>
+            <Field
+              className={s.checkInput}
+              name={`ball-${index}`}
+              component="input"
+              type="checkbox"
+            />
+            {ball}
+          </label>
+        ))}
+        <div className={s.inputHeading}>Есть ли у Вас пара на бал?</div>
+        {this.booleanChoises.map((choice, index) => (
+          <label className={s.choiseLabel} key={index}>
+            <Field className={s.checkInput} name="has-partner" component="input" type="radio" value={`${index === 0}`}>
+            </Field>
+            {choice}
+          </label>
+        ))}
+        <div className={s.inputHeading}>Хотите ли Вы быть дебютантом бала?</div>
+        {this.booleanChoises.map((choice, index) => (
+          <label className={s.choiseLabel} key={index}>
+            <Field className={s.checkInput} name="debut" component="input" type="radio" value={`${index === 0}`}>
+            </Field>
+            {choice}
+          </label>
+        ))}
+        <div>
+          <button className={s.button} type="submit" disabled={submitting}>Зарегистрироваться!</button>
+        </div>
+      </form>
+    )
+  }
+}
+
 const validate = values => {
   const requiredFields = [
     'name',
@@ -11,7 +96,7 @@ const validate = values => {
     'email',
     'number',
     'vk'
-  ];
+  ]
   const errors = {}
 
   requiredFields.forEach((fieldName) => {
@@ -27,36 +112,9 @@ const validate = values => {
   return errors
 }
 
-const RegisterForm = (props) => {
-  const { handleSubmit, pristine, reset, submitting } = props
-  const skillLevels = ['Новичек', 'Любитель', 'Профи'];
-  return (
-    <form onSubmit={handleSubmit} noValidate >
-      <Field name="name" type="text" component={FormField} label="ФИО"/>
-      <Field name="date" type="date" component={FormField} label="Дата рождения"/>
-      <Field name="number" type="number" component={FormField} label="Телефон"/>
-      <Field name="email" type="email" component={FormField} label="Email"/>
-      <Field name="vk" component={FormField} label="Ссылка на профиль вконтакте (для добавления в группу)"/>
-      <div>Уровень танцевальной подготовки</div>
-      {skillLevels.map((level, index) => (
-        <label className={s.label} key={index}>
-          <Field className={s.radio} name="level" component="input" type="radio" value={`${index}`}>
-          </Field>
-          {level}
-        </label>
-      ))}
-      <div>
-        <button className={s.button} type="submit" disabled={submitting}>Зарегистрироваться!</button>
-      </div>
-    </form>
-  )
-}
+RegisterForm = reduxForm({
+  form: 'RegisterForm',
+  validate
+})(RegisterForm)
 
-RegisterForm.propTypes = {
-
-}
-
-export default reduxForm({
-  form: 'RegisterForm',  // a unique identifier for this form
-  validate                // <--- validation function given to redux-form
-})(withStyles(s)(RegisterForm))
+export default withStyles(s)(RegisterForm)
